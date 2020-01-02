fb5a4149bc5f3de1a4d7198ab9f304032c872613
abuzar hamza <abuzarhamza@gmail.com>
2020-01-02 11:24:27 -0600

    updated the download flow

--- automation/downloadTafseerMp3.js
+++ automation/downloadTafseerMp3.js
@@ -2,20 +2,66 @@ const fs = require('fs')
     readline = require('readline')
     path = require('path'),
     http = require('http'),
-    asyncForEach = require('async/forEachSeries');
+    _ = require('lodash'),
+    mp3Duration = require('mp3-duration');
+    ;
 
+const downloadFile = (url, dest) => {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest, { flags: "wx" });
 
-const downloadFile = async (url, destination) => {
+        const request = http.get(url, response => {
+            if (response.statusCode === 200) {
+                response.pipe(file);
+            } else {
+                file.close();
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
+            }
+        });
 
-    var file = fs.createWriteStream(dest);
-    var request = http.get(url, function (response) {
-        response.pipe(file);
-        file.on('finish', function () {
-            file.close(cb);
+        request.on("error", err => {
+            file.close();
+            fs.unlink(dest, () => {}); // Delete temp file
+            reject(err.message);
+        });
+
+        file.on("finish", () => {
+            resolve();
+        });
+
+        file.on("error", err => {
+            file.close();
+
+            if (err.code === "EEXIST") {
+                reject("File already exists");
+            } else {
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(err.message);
+            }
         });
     });
 };
+    
 
+/* const downloadFile = async (url, destination) => {
+
+    var file = fs.createWriteStream(destination);
+    var request = http.get(url, (response) => {
+        if () {
+
+        }
+        response.pipe(file);
+        file.on('finish', function () {
+            file.close(cb);
+        }
+        ).on('error', (err) => {             
+            fs.unlink(destination); 
+            if (cb) cb(err.message);
+          });;
+    });
+};
+ */
 const readInputFile = async (fileName) => {
 
     const fileInput = [];
@@ -97,12 +143,46 @@ const getMp3FileNameFromUrl = async (url) => {
     return mp3FileName;
 };
 
+const getMp3Duration = async (filePath) => {
+    return new Promise((resolve, reject) => {
+        mp3Duration(filePath, (err, duration) {
+            if (err) reject(err.message);
+            //console.log('Your file is ' + duration + ' seconds long');
+            resolve(duration);
+        });
+    });
+};
+
 // read the input file
 // read the out put dir
 // check if the file already exists
 //  if yes generate the stats for mp3
 // no file found , download the file , 
 // generate the state of the file
+const timeConverter =  (number, unit) => {
+    let d, h, m, s
+  
+    if (isNaN(number)) {
+      throw new TypeError('Value sent to seconds-converter must be a number.')
+    }
+  
+    if (unit === 'sec' || unit === 'seconds') {
+      s = number
+    } else if (unit === 'ms' || unit === 'milliseconds' || !unit) {
+      s = Math.floor(number / 1000)
+    } else {
+      throw new TypeError('Unit must be \'sec\' or \'ms\'');
+    }
+  
+    m = Math.floor(s / 60)
+    s = s % 60
+    h = Math.floor(m / 60)
+    m = m % 60
+    d = Math.floor(h / 24)
+    h = h % 24
+  
+    return {days: d, hours: h, minutes: m, seconds: s}
+};
 
 ( async()=> {
 
@@ -112,26 +192,55 @@ const getMp3FileNameFromUrl = async (url) => {
         const jsonFileName = "";
 
         const fileInput = await readInputFile('./mp3DownloadLink123.txt');
-        const outputDirFiles = await readOutputDir("");
+        const outputDirFiles = await readOutputDir(outputDir);
         const jsonObject = await readJsonStatFile(path.join(outputDir, jsonFileName));
-        const filemp3 = await filterMp3FileName(outputDirFiles);
-        forEachSeries(fileInput, (urlLink, next)=> {
-            //check the file in the dir
-            let fileName = await getMp3FileNameFromUrl(urlLink);
-            if (!fileName) return next(fileName);
-            if (!jsonObject[fileName]) {
-                // download it if not available
+        const filemp3s = await filterMp3FileName(outputDirFiles);
+        for(const urlLink of fileInput) {
 
+            const fileName = await getMp3FileNameFromUrl(urlLink);
+            if ( !fileName) {
+                console.log(`msg : could not get fileName ${fileName} for ${urlLink}`);
+                continue;
             }
-            
-            
+            if  ( !jsonObject[fileName] ) {
 
-            // get the stats
+                //check the file in the dir
+                if (_.includes(filemp3s, fileName)) {
+                    console.log(`msg : ${fileName} is available}`);
+                    continue;
+                }
+                const errorMsg = await downloadFile(urlLink, path.join(outputDirFiles, fileName));
+                if (errorMsg) {
+                    console.log(`error : for file ${fileName} ${errorMsg}`);
+                    continue;
+                }
+                //get stats
+                const stats = fs.statSync(path.join(outputDirFiles, fileName));
+                const fileSizeInBytes = stats.size;
+                //add stats obj to jsonObj
+                const durationInMiliSec  = await getMp3Duration(path.join(outputDirFiles, fileName));
+                if (durationInMiliSec instanceof Error) {
+                    continue;
+                }
+                const durationInSec =  Math.floor(durationInMiliSec / 1000);
+                const timeObj = timeConverter(durationInMiliSec, 'ms');
+                if (timeObj instanceof Error) continue;
 
-            //write the json file
-        }, (err)=> {
+                const jsonObj = {
+                    file_name: fileName,
+                    duration_in_sec: durationInSec,
+                    duration_in_mili_sec: durationInMiliSec,
+                    time : `${timeObj.hours}:${timeObj.minutes}:${timeObj.seconds}`,
+                };
 
-        });
+                //write the jsonObj to the file
+
+                console.log(jsonObj);
+                
+            }
+
+
+        }
 
     } catch(err) {
         console.log(`Error : ${err}`);
